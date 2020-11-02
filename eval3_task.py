f473def460f80b641b79e2532989e3a8921f7753
Chengxi Yang <yangchen@oregonstate.edu>
2020-11-02 12:24:33 -0800

    Add object preference dataset

--- eval3_task.py
+++ eval3_task.py
@@ -11,4 +11,16 @@ if __name__ == "__main__":
         task="eval3_dataset", scene_type="TRAINING_SINGLE_OBJECT", seed=50,
         start_scene_number=start_scene_number, frame_collector=None, set_trophy=False
     )
-    env.reset()
+
+    for scene in range(len(env.all_scenes) - start_scene_number):
+        env.reset(random_init=False)
+        print("\n")
+        print(scene + start_scene_number)
+        for i, x in enumerate(env.scene_config['goal']['action_list']):
+            if x[0] != "Pass":
+                print(x[0])
+            env.step(action=x[0])
+            if env.step_output is None:
+                break
+        exit(0)
+
--- get_dataset.sh
+++ get_dataset.sh
@@ -1,31 +1,31 @@
 #!/bin/bash -x
 
-#wget https://evaluation2-training-scenes.s3.amazonaws.com/interaction-scenes.zip
-#wget https://evaluation2-training-scenes.s3.amazonaws.com/intphys-scenes.zip
-#
-#unzip interaction-scenes.zip
-#unzip intphys-scenes.zip -d intphys_scenes
-#
-#mkdir intphys_scenes/gravity
-#mkdir intphys_scenes/object_permanence
-#mkdir intphys_scenes/shape_constancy
-#mkdir intphys_scenes/spatio_temporal_continuity
-#
-#mkdir interaction_scenes/retrieval
-#mkdir interaction_scenes/traversal
-#mkdir interaction_scenes/transferral
-#
-#mv intphys_scenes/gravity*.json intphys_scenes/gravity
-#mv intphys_scenes/object_permanence*.json intphys_scenes/object_permanence
-#mv intphys_scenes/shape_constancy*.json intphys_scenes/shape_constancy
-#mv intphys_scenes/spatio_temporal_continuity*.json intphys_scenes/spatio_temporal_continuity
-#
-#mv interaction_scenes/retrieval*.json interaction_scenes/retrieval
-#mv interaction_scenes/traversal*.json interaction_scenes/traversal
-#mv interaction_scenes/transferral*.json interaction_scenes/transferral
-#
-#rm interaction-scenes.zip
-#rm intphys-scenes.zip
+wget https://evaluation2-training-scenes.s3.amazonaws.com/interaction-scenes.zip
+wget https://evaluation2-training-scenes.s3.amazonaws.com/intphys-scenes.zip
+
+unzip interaction-scenes.zip
+unzip intphys-scenes.zip -d intphys_scenes
+
+mkdir intphys_scenes/gravity
+mkdir intphys_scenes/object_permanence
+mkdir intphys_scenes/shape_constancy
+mkdir intphys_scenes/spatio_temporal_continuity
+
+mkdir interaction_scenes/retrieval
+mkdir interaction_scenes/traversal
+mkdir interaction_scenes/transferral
+
+mv intphys_scenes/gravity*.json intphys_scenes/gravity
+mv intphys_scenes/object_permanence*.json intphys_scenes/object_permanence
+mv intphys_scenes/shape_constancy*.json intphys_scenes/shape_constancy
+mv intphys_scenes/spatio_temporal_continuity*.json intphys_scenes/spatio_temporal_continuity
+
+mv interaction_scenes/retrieval*.json interaction_scenes/retrieval
+mv interaction_scenes/traversal*.json interaction_scenes/traversal
+mv interaction_scenes/transferral*.json interaction_scenes/transferral
+
+rm interaction-scenes.zip
+rm intphys-scenes.zip
 
 mkdir -p eval3_dataset
 wget https://evaluation-training-scenes.s3.amazonaws.com/eval3/training-single-object.zip
