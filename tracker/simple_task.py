07077bf87aa2accd96dfb6c047107b67264062e0
yuanjial <yuanjial@oregonstate.edu>
2020-11-11 14:41:00 -0800

    new branch for generating instseg data

--- tracker/simple_task.py
+++ tracker/simple_task.py
@@ -6,20 +6,17 @@ from tracker.frame_collector import Frame_collector
 
 
 if __name__ == "__main__":
-    start_scene_number, end_scene_number = 500, -1
+    start_scene_number, end_scene_number = 0, 100
     task, scene_type = "interaction_scenes", "transferral"
     collector = Frame_collector(scene_dir="instSeg/"+task+"/"+scene_type, start_scene_number=start_scene_number)
     env = McsEnv(
         task=task, scene_type=scene_type, seed=50,
-        start_scene_number=start_scene_number, frame_collector=collector, set_trophy=True
+        start_scene_number=start_scene_number, frame_collector=collector, set_trophy=False
     )
     metaController = MetaController(env)
 
     if end_scene_number == -1:
         end_scene_number = len(env.all_scenes)
-    else:
-        end_scene_number = min(end_scene_number, len(env.all_scenes))
-
     while env.current_scene < end_scene_number - 1:
         env.reset()
         result = metaController.excecute()
