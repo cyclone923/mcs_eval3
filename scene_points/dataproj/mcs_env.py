2ec1d6fe9f9f848943a257411fb394b572b9c176
Erich Merrill <erich@erichmerrill.com>
2020-11-16 14:21:39 -0800

    Updated projection example to work with new sim version

--- scene_points/dataproj/mcs_env.py
+++ scene_points/dataproj/mcs_env.py
@@ -7,15 +7,17 @@ Modified from Chengxi's McsEnv wrapper
 
 from pathlib import Path
 import os
-import machine_common_sense
+import machine_common_sense as mcs
 import pickle
 
 class McsEnv:
     def __init__(self, base, scenes, filter=None):
         base = Path(base)
+        scenes = Path(scenes)
         os.environ['MCS_CONFIG_FILE_PATH'] = str(base/'mcs_config.yaml')
-        app = base/'MCS-AI2-THOR-Unity-App-v0.1.0.x86_64'
-        self.controller = machine_common_sense.MCS.create_controller(str(app))
+        app = base/'MCS-AI2-THOR-Unity-App-v0.3.3.x86_64'
+        self.controller = mcs.create_controller(str(app), depth_maps=True,
+                                                object_masks=True)
         self.read_scenes(scenes, filter)
 
     def read_scenes(self, scenedir, filter):
@@ -26,23 +28,18 @@ class McsEnv:
             self.all_scenes = [s for s in _scenegen if filter in s.name]
 
     def run_scene(self, scene_path):
-        self.scene_config, _ = machine_common_sense.MCS.load_config_json_file(scene_path)
-        step_output = self.controller.start_scene(self.scene_config)
-        yield step_output
-        for action in self.scene_config['goal']['action_list']:
+        scene_config, _ = mcs.load_config_json_file(scene_path)
+        step_output = self.controller.start_scene(scene_config)
+        for action in scene_config['goal']['action_list']:
             step_output = self.controller.step(action=action[0])
             yield step_output
 
 if __name__ == '__main__':
-    env = McsEnv(base='data/thor')
+    env = McsEnv(base='data/thor', scenes='data/thor/scenes')
     print(len(env.all_scenes))
     for i, scene_path in enumerate(env.all_scenes):
         outs = []
-        print(f'Scene {i}')
+        print(scene_path)
         for step_output in env.run_scene(scene_path):
             outs.append(step_output)
-            print('step')
-        with open('test.pkl', 'wb') as fd:
-            pickle.dump((env.scene_config, outs), fd)
-        break
     print('Done!')
