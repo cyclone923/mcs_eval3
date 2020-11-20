ca33620218b39ce5e4f1801cef2b908cd36c2dea
rajesh <rajeshdm9@gmail.com>
2020-11-19 20:19:25 -0800

    pickup object bug fix

--- simple_task.py
+++ simple_task.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     start_scene_number = 0
     collector = Frame_collector(scene_dir="simple_task_img", start_scene_number=start_scene_number)
     env = McsEnv(
-        #task="interaction_scenes", scene_type="retrieval", seed=50,
+        task="interaction_scenes", scene_type="retrieval", seed=50,
         #task="interaction_scenes", scene_type="traversal", seed=50,
         #task="interaction_scenes", scene_type="transferral", seed=50,
-        task="interaction_scenes", scene_type="experiment", seed=50,
+        #task="interaction_scenes", scene_type="experiment", seed=50,
         #start_scene_number=start_scene_number, frame_collector=None, set_trophy=True, trophy_prob=1
         start_scene_number=start_scene_number, frame_collector=None, set_trophy=False, trophy_prob=1
     ) # trophy_prob=1 mean the trophy is 100% outside the box, trophy_prob=0 mean the trophy is 100% inside the box,
