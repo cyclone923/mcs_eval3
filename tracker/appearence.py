940b1ef764a702d0f13240361173aefea6026ca8
Erich Merrill <erich@erichmerrill.com>
2020-12-06 11:49:47 -0800

    Revert "Removed training code from appearance model"
    This reverts commit 104ecc9b4bbe343ed5163f37013ca8a2d48f815a.

--- tracker/appearence.py
+++ tracker/appearence.py
@@ -3,16 +3,18 @@ import gzip
 from pathlib import Path
 from argparse import ArgumentParser
 import os
-from .utils import draw_bounding_boxes, draw_appearance_bars, split_obj_masks, get_obj_position, get_mask_box
+from utils import draw_bounding_boxes, draw_appearance_bars, split_obj_masks, get_obj_position, get_mask_box
 
-from .track import track_objects
+from track import track_objects
 import torch
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
 
 from torch.optim import Adam
+from torch.utils.tensorboard import SummaryWriter
 import torch.nn as nn
 import cv2
+from tqdm import tqdm
 import webcolors
 
 
@@ -166,3 +168,253 @@ def object_appearance_match(model, image, objects_info, device='cpu'):
         objects_info[obj_key]['appearance']['match'] = shape_matches and color_matches
 
     return objects_info
+
+
+def process_video(video_data, appearance_model, save_path=None, save_mp4=False, device='cpu'):
+    track_info = {}
+    processed_frames = []
+    for frame_num, frame in enumerate(video_data):
+        track_info = track_objects(frame.obj_mask, track_info)
+        track_info['objects'] = object_appearance_match(appearance_model, frame.image,
+                                                        track_info['objects'], device)
+
+        img = draw_bounding_boxes(frame.image, track_info['objects'])
+        img = draw_appearance_bars(img, track_info['objects'])
+        processed_frames.append(img)
+
+    # save gif
+    processed_frames[0].save(save_path + '.gif', save_all=True,
+                             append_images=processed_frames[1:], optimize=False, loop=1)
+
+    # save video
+    if save_mp4:
+        import moviepy.editor as mp
+        clip = mp.VideoFileClip(save_path + '.gif')
+        clip.write_videofile(save_path + '.mp4')
+        os.remove(save_path + '.gif')
+
+
+class ObjectDataset(Dataset):
+    """ Dataset of objects with labels indictating their shape"""
+
+    def __init__(self, data):
+        self.data = data
+        self.shape_labels = sorted(set(data['shapes']))
+        self.color_labels = sorted(set(np.array(data['textures']).squeeze(1)))
+
+        self.data['shapes'] = np.array(self.data['shapes'])
+        self.data['color'] = np.array(data['textures']).squeeze(1)
+
+        for shape_id, shape in enumerate(self.shape_labels):
+            self.data['shapes'][self.data['shapes'] == shape] = shape_id
+
+        for color_id, color in enumerate(self.color_labels):
+            self.data['color'][self.data['color'] == color] = color_id
+
+        self.data['shapes'] = self.data['shapes'].astype(np.int)
+        self.data['color'] = self.data['color'].astype(np.int)
+
+    def shape_label_name(self, label_id):
+        return self.shape_labels[label_id]
+
+    def shape_labels_count(self):
+        _labels, counts = np.unique(self.data['shapes'], return_counts=True)
+        return [counts[_labels == label_i][0] for label_i, _ in enumerate(self.shape_labels)]
+
+    def color_label_name(self, label_id):
+        return self.color_labels[label_id]
+
+    def color_labels_count(self):
+        _labels, counts = np.unique(self.data['color'], return_counts=True)
+        return [counts[_labels == label_i][0] for label_i, _ in enumerate(self.shape_labels)]
+
+    def get_dataset_weights(self, label_probs):
+        weights = np.random.randn(self.__len__())
+        for label_i, prob in enumerate(label_probs):
+            weights[self.data['shapes'] == label_i] = prob
+        return weights
+
+    def __len__(self):
+        return len(self.data['images'])
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        return {'images': self.data['images'][idx],
+                'shapes': self.data['shapes'][idx],
+                'color': self.data['color'][idx]}
+
+
+def generate_data(scenes_files):
+    data = {'images': [], 'shapes': [], 'materials': [], 'textures': []}
+    for scene_file in tqdm(sorted(scenes_files)):
+        with gzip.open(scene_file, 'rb') as fd:
+            scene_data = pickle.load(fd)
+
+        for frame_num, frame in enumerate(scene_data):
+
+            objs = frame.obj_data
+            obj_masks = split_obj_masks(frame.obj_mask, len(objs))
+
+            for obj_i, (obj, obj_mask) in enumerate(zip(objs, obj_masks)):
+                if True not in obj_mask:
+                    # Remove any object which doesn't have a valid mask.
+                    print('Empty Mask found. It will be ignored for scene processing')
+                    objs.remove(obj)
+                    del obj_masks[obj_i]
+                else:
+                    (top_left_x, top_left_y), (bottom_right_x, bottom_right_y) = get_mask_box(obj_mask)
+                    obj_image = frame.image.crop((top_left_y, top_left_x, bottom_right_y, bottom_right_x))
+
+                    # Todo: Think again about this re-sizing
+                    # this is to ensure all images have same size.
+                    obj_image = obj_image.resize((50, 50))
+                    obj_image = np.array(obj_image).reshape(3, 50, 50)  # Because channels comes first for Conv2d
+                    data['images'].append(np.array(obj_image))
+                    data['shapes'].append(obj.shape)
+                    data['materials'].append(obj.material_list)
+                    data['textures'].append(obj.texture_color_list)
+
+    for x in data:
+        data[x] = np.array(data[x])
+
+    print('Len of Dataset:', len(data['images']))
+    return data
+
+
+def train_appearance_matching(dataloader, model, optimizer, epochs: int, writer, checkpoint_path,
+                              checkpoint_interval: int = 1, log_interval: int = 1,
+                              restore_checkpoint=False):
+    if restore_checkpoint:
+        checkpoint = torch.load(restore_checkpoint)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        init_epoch = checkpoint['epoch']
+    else:
+        init_epoch = 0
+
+    model.train()
+    for epoch in range(init_epoch, epochs):
+        batch_losses = {'shape': [], 'color': []}
+        batch_acc = {'shape': [], 'color': []}
+        for i_batch, batch in enumerate(dataloader):
+            object_image = batch['images']
+            object_shape = batch['shapes']
+            object_color = batch['color']
+
+            object_image = object_image.float()
+            feature, shape_logits, color_logits = model(object_image)
+
+            shape_loss = nn.CrossEntropyLoss()(shape_logits, object_shape)
+            color_loss = nn.CrossEntropyLoss()(color_logits, object_color)
+            shape_acc = sum(torch.argmax(shape_logits, dim=1) == object_shape).item() / len(object_shape)
+            color_acc = sum(torch.argmax(color_logits, dim=1) == object_color).item() / len(object_color)
+            batch_losses['shape'].append(shape_loss.item())
+            batch_acc['shape'].append(shape_acc)
+            batch_losses['color'].append(color_loss.item())
+            batch_acc['color'].append(color_acc)
+
+            # optimize
+            optimizer.zero_grad()
+            (shape_loss + color_loss).backward()
+            optimizer.step()
+
+        writer.add_scalar('train/shape_loss', np.mean(batch_losses['shape']), epoch)
+        writer.add_scalar('train/shape_accuracy', np.mean(batch_acc['shape']), epoch)
+        writer.add_scalar('train/color_loss', np.mean(batch_losses['color']), epoch)
+        writer.add_scalar('train/color_accuracy', np.mean(batch_acc['color']), epoch)
+
+        if (epoch % checkpoint_interval) == 0:
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+            }, checkpoint_path)
+
+            # save model separately as well
+            torch.save(model.state_dict(), model_path)
+
+        if epoch % log_interval == 0:
+            print('Epoch: {} Shape Loss: {} Shape Accuracy:{} Color Acc: {}'.format(
+                epoch, np.mean(batch_losses['shape']), np.mean(batch_acc['shape']),
+                np.mean(batch_acc['color'])))
+
+
+def make_parser():
+    parser = ArgumentParser()
+    parser.add_argument('--test-scenes-path', required=True, type=Path)
+    parser.add_argument('--train-scenes-path', required=True, type=Path)
+    parser.add_argument('--train-dataset-path', required=False, type=Path,
+                        default=os.path.join(os.getcwd(), 'train_object_dataset.p'))
+    parser.add_argument('--test-dataset-path', required=False, type=Path,
+                        default=os.path.join(os.getcwd(), 'test_object_dataset.p'))
+    parser.add_argument('--batch-size', required=False, type=int, default=32)
+    parser.add_argument('--results-dir', required=False, type=Path, default=os.path.join(os.getcwd(), 'results'))
+    parser.add_argument('--run', required=False, type=int, default=1)
+    parser.add_argument('--lr', required=False, type=float, default=0.001)
+    parser.add_argument('--epochs', required=False, type=int, default=50)
+    parser.add_argument('--checkpoint-interval', required=False, type=int, default=1)
+    parser.add_argument('--log-interval', required=False, type=int, default=1)
+    parser.add_argument('--opr', choices=['generate_dataset', 'train', 'demo'], default='demo',
+                        help='operation (opr) to be performed')
+
+    return parser
+
+
+if __name__ == '__main__':
+    args = make_parser().parse_args()
+    args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    # paths
+    experiment_path = os.path.join(args.results_dir, 'run_{}'.format(args.run), )
+    os.makedirs(experiment_path, exist_ok=True)
+    log_path = os.path.join(experiment_path, 'logs')
+    model_path = os.path.join(experiment_path, 'model.p')
+    checkpoint_path = os.path.join(experiment_path, 'checkpoint.p')
+
+    # Determine the operation to be performed
+    if args.opr == 'generate_dataset':
+        train_scenes = list(args.train_scenes_path.glob('*.pkl.gz'))
+        data = generate_data(train_scenes)
+        pickle.dump(data, open(args.train_dataset_path, 'wb'))
+
+        test_scenes = list(args.test_scenes_path.glob('*.pkl.gz'))
+        data = generate_data(test_scenes)
+        pickle.dump(data, open(args.test_dataset_path, 'wb'))
+
+    elif args.opr == 'train':
+        # flush every 1 minutes
+        summary_writer = SummaryWriter(log_path, flush_secs=60 * 1)
+
+        # create balanced distribution
+        train_object_dataset = ObjectDataset(pickle.load(open(args.train_dataset_path, 'rb')))
+        probs = 1 / torch.Tensor(train_object_dataset.shape_labels_count())
+        weights = train_object_dataset.get_dataset_weights(probs)
+        sampler = torch.utils.data.sampler.WeightedRandomSampler(weights, len(weights), replacement=True)
+        dataloader = DataLoader(train_object_dataset, batch_size=args.batch_size, shuffle=False, num_workers=1,
+                                sampler=sampler)
+
+        model = AppearanceMatchModel()
+        optimizer = Adam(model.parameters(), lr=args.lr)
+
+        # train
+        train_appearance_matching(dataloader, model, optimizer, args.epochs, summary_writer,
+                                  checkpoint_path, args.checkpoint_interval, args.log_interval)
+
+    elif args.opr == 'demo':
+        all_scenes = list(args.test_scenes_path.glob('*.pkl.gz'))
+        print(f'Found {len(all_scenes)} scenes')
+
+        model = AppearanceMatchModel()
+
+        model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+        model = model.to(args.device)
+
+        for scene_file in all_scenes:
+            with gzip.open(scene_file, 'rb') as fd:
+                scene_data = pickle.load(fd)
+
+            print(f'{scene_file.name}')
+            process_video(scene_data, model, os.path.join(os.getcwd(), scene_file.name), save_mp4=True,
+                          device=args.device)
