c619cbc287f0495596a1072d035e4447ab6bdc0c
rajesh <rajeshdm9@gmail.com>
2020-12-07 20:00:47 -0800

    updating setup_vision.sh to download voe model as well

--- setup_vision.sh
+++ setup_vision.sh
@@ -5,4 +5,7 @@ cd ../../
 wget https://oregonstate.box.com/shared/static/0syjouwkkpm0g1zbnt1riheshfvtd2by.pth
 mv 0syjouwkkpm0g1zbnt1riheshfvtd2by.pth dvis_resnet50_mc.pth
 
+wget https://oregonstate.box.com/shared/static/zmvcjyumltkziqfqbcqodkh6dgecikci.pth
+mv zmvcjyumltkziqfqbcqodkh6dgecikci.pth dvis_resnet50_mc_voe.pth 
+
 cd ../../
