6115f3d4f75f060e4fd783969cd85c85f412cb1e
Erich Merrill <erich@erichmerrill.com>
2020-12-07 21:12:30 -0800

    Updated vision setup

--- setup_vision.sh
+++ setup_vision.sh
@@ -5,7 +5,8 @@ cd ../../
 wget https://oregonstate.box.com/shared/static/0syjouwkkpm0g1zbnt1riheshfvtd2by.pth
 mv 0syjouwkkpm0g1zbnt1riheshfvtd2by.pth dvis_resnet50_mc.pth
 
+cd ../../visionmodule
 wget https://oregonstate.box.com/shared/static/zmvcjyumltkziqfqbcqodkh6dgecikci.pth
 mv zmvcjyumltkziqfqbcqodkh6dgecikci.pth dvis_resnet50_mc_voe.pth 
 
-cd ../../
+cd ../
