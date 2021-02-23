0bfbd94e6813c85906e75a4110d8799e452df783
RahulDamineni <daminenirahul@gmail.com>
2021-02-23 05:52:25 -0800

    cleanup refactored code

--- vision/gravity.py
+++ vision/gravity.py
@@ -29,19 +29,6 @@ OBJ_ROLES = {
     "floor"
 }
 
-def clean_object_mask(obj_mask, depth_mask):
-    '''
-    Uses depth_mask to return front view of objects in obj_mask
-    '''
-    pass
-
-
-def extract_object_trajectories(temporal_rgbd_data):
-    '''
-    First segments objects, then classifies object kind, 
-    tracks and returns trajectories
-    '''
-    pass
 
 @dataclass
 class Object:
@@ -53,7 +40,7 @@ class Object:
 
         self.extract_physical_props()
         self.find_obj_kind()
-        
+
         assert self.kind in OBJ_KINDS, f"Object creation failed! Unrecognised kind: {self.kind}"
         assert self.role in OBJ_ROLES, f"Object creation failed! Unrecognised role: {self.role}"
 
@@ -68,6 +55,7 @@ class Object:
         Takes a binary blob and returns one among OBJ_KINDS: sets `kind`
         '''
         pass
+
 
 @dataclass
 class L2DataPacket:
