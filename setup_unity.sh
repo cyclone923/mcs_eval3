806ef018c4fcbc947b63ced8bab53da769d68428
dcharrezt@unsa.edu.pe <dcharrezt@unsa.edu.pe>
2021-02-25 20:40:27 +0000

    Updated mcs3.5 to mcs 3.8, mcs@latest was deprecated and current mcs3.8 is installed from  mcs@master

--- setup_unity.sh
+++ setup_unity.sh
@@ -5,18 +5,18 @@ cd unity_app
 
 if [ "$(uname)" == "Darwin" ]; then
     # Do something under Mac OS X platform
-    wget https://github.com/NextCenturyCorporation/MCS/releases/download/0.3.5/MCS-AI2-THOR-Unity-App-v0.3.5-mac.zip
-    unzip MCS-AI2-THOR-Unity-App-v0.3.5-mac.zip
-    rm MCS-AI2-THOR-Unity-App-v0.3.5-mac.zip
+    wget https://github.com/NextCenturyCorporation/MCS/releases/download/0.3.8/MCS-AI2-THOR-Unity-App-v0.3.8-mac.zip
+    unzip MCS-AI2-THOR-Unity-App-v0.3.8-mac.zip
+    rm MCS-AI2-THOR-Unity-App-v0.3.8-mac.zip
     echo unity_path: \'unity_app/MCSai2thor.app/Contents/MacOS/MCSai2thor\' > ../unity_path.yaml
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Do something under GNU/Linux platform
-    wget https://github.com/NextCenturyCorporation/MCS/releases/download/0.3.5/MCS-AI2-THOR-Unity-App-v0.3.5.x86_64
-    wget https://github.com/NextCenturyCorporation/MCS/releases/download/0.3.5/MCS-AI2-THOR-Unity-App-v0.3.5_Data.tar.gz
-    tar -xzvf MCS-AI2-THOR-Unity-App-v0.3.5_Data.tar.gz
-    rm MCS-AI2-THOR-Unity-App-v0.3.5_Data.tar.gz
-    chmod a+x MCS-AI2-THOR-Unity-App-v0.3.5.x86_64
-    echo unity_path: \'unity_app/MCS-AI2-THOR-Unity-App-v0.3.5.x86_64\' > ../unity_path.yaml
+    wget https://github.com/NextCenturyCorporation/MCS/releases/download/0.3.8/MCS-AI2-THOR-Unity-App-v0.3.8.x86_64
+    wget https://github.com/NextCenturyCorporation/MCS/releases/download/0.3.8/MCS-AI2-THOR-Unity-App-v0.3.8_Data.tar.gz
+    tar -xzvf MCS-AI2-THOR-Unity-App-v0.3.8_Data.tar.gz
+    rm MCS-AI2-THOR-Unity-App-v0.3.8_Data.tar.gz
+    chmod a+x MCS-AI2-THOR-Unity-App-v0.3.8.x86_64
+    echo unity_path: \'unity_app/MCS-AI2-THOR-Unity-App-v0.3.8.x86_64\' > ../unity_path.yaml
 
 fi
 
