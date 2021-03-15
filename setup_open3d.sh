ca50c45f8f1cc1b453a3d4b5757f6711c869f5ee
RahulDamineni <daminenirahul@gmail.com>
2021-03-15 01:13:30 -0700

    simplified open3D installation to circumvent complexities with installation from source

--- setup_open3d.sh
+++ setup_open3d.sh
@@ -1,25 +1,21 @@
 #!/bin/bash -x
 
 # Expects appropriate virtualenv is activated and $(which python) point to it
-# If the script breaks, install manually from http://www.open3d.org/docs/release/compilation.html
 
-git clone --recursive https://github.com/intel-isl/Open3D
-
-cd Open3D
-mkdir build
+# If the script breaks, 
+# 1. Check if you are using the right dev wheel for your corresponding Python version here:
+# http://www.open3d.org/docs/latest/getting_started.html
+# 2. Or install from source: http://www.open3d.org/docs/release/compilation.html
 
 if [ "$(uname)" == "Darwin" ]; then
     # Do something under Mac OS X platform
-    cmake -DCMAKE_INSTALL_PREFIX=./build ..
-    make -j$(sysctl -n hw.physicalcpu)
-    make install-pip-package
-    python -c "import open3d"
+    # For python 3.8
+    pip install --user https://storage.googleapis.com/open3d-releases-master/python-wheels/open3d-0.12.0+50148ce-cp38-cp38-macosx_10_14_x86_64.whl
 
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Do something under GNU/Linux platform
-    util/install_deps_ubuntu.sh
-    cmake -DCMAKE_INSTALL_PREFIX=./build ..
-    make -j$(sysctl -n hw.physicalcpu)
-    make install-pip-package
-    python -c "import open3d"
+    # For python 3.6
+    pip install --user https://storage.googleapis.com/open3d-releases-master/python-wheels/open3d-0.12.0+50148ce-cp36-cp36m-linux_x86_64.whl
 fi
+
+python -c "import open3d"
