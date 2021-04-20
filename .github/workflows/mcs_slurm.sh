bdf77881f94ec1d9fe9225ddcfef32c8076f8bdf
Robert Bain <31903812+bainro@users.noreply.github.com>
2021-04-20 16:14:43 -0700

    Update mcs_slurm.sh

--- .github/workflows/mcs_slurm.sh
+++ .github/workflows/mcs_slurm.sh
@@ -7,8 +7,8 @@
 #SBATCH --gres=gpu:1     # request 1 GPU
 #SBATCH --nodelist=cn-gpu2
  
-# sleep time must match cleanup time below!
-srun -N1 -n1 sleep 1e4 & # ~3 hrs
+MAX_TIME = 1e4 # ~ 3hrs
+srun -N1 -n1 sleep $MAX_TIME &
 module load gcc/6.5
 module load cuda
 nvidia-smi
@@ -16,27 +16,32 @@ cd /scratch/MCS
 
 # if conda not setup
 if ! [ -d "miniconda3" ]; then
- mkdir -p miniconda3
- wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda3/miniconda.sh
- bash miniconda3/miniconda.sh -b -u -p miniconda3
- miniconda3/bin/conda init bash
- conda create -n mcs_opics python=3.6.8  
+ mkdir -p miniconda3 &>/dev/null
+ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda3/miniconda.sh &>/dev/null
+ bash miniconda3/miniconda.sh -b -u -p miniconda3 &>/dev/null
+ miniconda3/bin/conda init bash &>/dev/null
+ conda create -n mcs_opics python=3.6.8 &>/dev/null
 fi
-conda activate mcs_opics
-python -V;exit
+conda activate mcs_opics &>/dev/null
+python -V
 
+# get git repo, cd to it, select specific branch
 rm -fr mcs_opics
-git clone https://github.com/MCS-OSU/mcs_opics.git
-cd mcs_opics
-git checkout test.yaml-rob
-git pull
+git clone https://github.com/MCS-OSU/mcs_opics.git &>/dev/null
+cd mcs_opics 
+git checkout test.yaml-rob 
+git pull &>/dev/null
+
 export OUR_XPID=`nvidia-smi | grep Xorg | awk '{print $5}'`
 export DISPLAY=`pgrep Xorg | xargs ps | grep $OUR_XPID | awk '{print $6}'`
-echo $DISPLAY
+echo "GPU's DISPLAY id"; printenv $DISPLAY
+
+# setup all the python env + dependencies
 pip install --upgrade pip
 pip install --upgrade setuptools
 pip install --upgrade wheel
-mkdir tmp -p # /tmp was full
+mkdir ./tmp -p 
+# /tmp on cn-gpu2 server can be full
 TMPDIR=/scratch/MCS/tmp python -m pip install --cache-dir /scratch/MCS/tmp --build /scratch/MCS/tmp git+https://github.com/NextCenturyCorporation/MCS@0.3.8
 pip install -r requirements.txt &> /dev/null
 pip show machine_common_sense
@@ -44,13 +49,15 @@ bash setup_torch.sh &> /dev/null
 bash setup_unity.sh &> /dev/null
 bash setup_vision.sh &> /dev/null
 export PYTHONPATH=$PWD
+
 python get_gravity_scenes.py &> /dev/null
 cp gravity_scenes/gravity_support_ex_01.json different_scenes
 # agent code in this branch explodes
 rm different_scenes/preference_0001_01.json
 python eval.py
+
 # will check output for this to confirm success!
 echo 69420
-# kill the process keeping the slurm job open
-# if you change the sleep time above, this needs changed too
-pgrep sleep | xargs ps | grep 1e4 | xargs echo | cut -d ' ' -f 1 | xargs kill
+
+# kill the process keeping this slurm job open
+pgrep sleep | xargs ps | grep $MAX_TIME | xargs echo | cut -d ' ' -f 1 | xargs kill
