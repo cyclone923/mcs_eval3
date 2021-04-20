1db756709bd98269a8465e436a489e3ca95e5788
Robert Bain <31903812+bainro@users.noreply.github.com>
2021-04-20 14:13:37 -0700

    Update mcs_submit.sh

--- .github/workflows/mcs_submit.sh
+++ .github/workflows/mcs_submit.sh
@@ -2,10 +2,7 @@
 source /etc/profile
 module load slurm
 
-RES=$(sbatch /tmp/mcs_slurm.sh)
-echo $RES;exit 3
-SJOBID=${RES##*}
-echo $SJOBID
+SJOBID=$(sbatch --parsable /tmp/mcs_slurm.sh)
 
 # wait for gpu job to finish
 until [ `sacct -j $SJOBID | grep COMPLETED | wc -l` -gt 0 -o `sacct -j $SJOBID | grep CANCELLED | wc -l` -gt 0 ]
