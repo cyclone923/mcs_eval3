d8a20651f700ced25f8b4e954e585a805f3567fa
Robert Bain <31903812+bainro@users.noreply.github.com>
2021-04-23 17:38:30 -0700

    Update and rename mcs_submit.sh to dvis_data_submit.sh

--- .github/workflows/dvis_data_submit.sh
+++ .github/workflows/dvis_data_submit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SJOBID=$(sbatch --parsable /tmp/mcs_slurm.sh)
+SJOBID=$(sbatch --parsable /tmp/dvis_data_slurm.sh)
 
 # wait for gpu job to finish
 until [ `sacct -j $SJOBID | grep COMPLETED | wc -l` -gt 0 -o `sacct -j $SJOBID | grep CANCELLED | wc -l` -gt 0 ]
@@ -10,11 +10,11 @@ done
 
 # copy output to github actions logs
 echo '==== STDOUT ===='
-cat mcs.out
+cat dvis_data.out
 echo '==== STDERR ===='
-cat mcs.err
+cat dvis_data.err
  
 # check if the build failed/passed
-if ! grep 69420 mcs.out; 
-  then exit 404; 
+if ! grep 69420 dvis_data.out;
+  then exit 404;
 fi
