dd8cb67dd0a3ef08d227a7de46ab96da41ad4895
Robert Bain <31903812+bainro@users.noreply.github.com>
2021-04-23 17:26:40 -0700

    Update mcs_slurm.sh

--- .github/workflows/mcs_slurm.sh
+++ .github/workflows/mcs_slurm.sh
@@ -37,7 +37,7 @@ if ! [ -d "mcs_opics" ]; then
 fi
 cd mcs_opics
 git stash
-git checkout test.yaml-rob 
+git checkout develop 
 git pull &>/dev/null
 
 export OUR_XPID=`nvidia-smi | grep Xorg | awk '{print $5}'`
