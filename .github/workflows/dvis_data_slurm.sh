58ccfe43eeb5f2592772653b827d34da52a2c0da
Diego Charrez <d.d.charrez.t@gmail.com>
2021-05-01 17:24:01 -0700

    updated workflows for training

--- .github/workflows/dvis_data_slurm.sh
+++ .github/workflows/dvis_data_slurm.sh
@@ -40,7 +40,7 @@ if ! [ -d "mcs_opics" ]; then
 fi
 cd mcs_opics
 git stash
-git checkout test.yaml-rob 
+git checkout component/interactive-env
 git pull &>/dev/null
 
 export OUR_XPID=`nvidia-smi | grep Xorg | awk '{print $5}'`
@@ -68,33 +68,53 @@ bash setup_vision.sh &> /dev/null
 bash get_dataset.sh &> /dev/null
 export PYTHONPATH=$PWD
 
+# Data Collecting
 # 66, 132, 198, 264, 330, 396, 462, 528, 594 and 660
-python vision/generateData/simple_task_multi.py 0 66 &
-python vision/generateData/simple_task_multi.py 132 66 & 
-python vision/generateData/simple_task_multi.py 198 66 &
-python vision/generateData/simple_task_multi.py 264 66 &
-python vision/generateData/simple_task_multi.py 330 66 &
-python vision/generateData/simple_task_multi.py 396 66 &
-python vision/generateData/simple_task_multi.py 462 66 &
-python vision/generateData/simple_task_multi.py 528 66 &
-python vision/generateData/simple_task_multi.py 660 66 &
-python vision/generateData/simple_task_multi.py 726 66 &
-python vision/generateData/simple_task_multi.py 792 66 &
-python vision/generateData/simple_task_multi.py 858 66 &
-python vision/generateData/simple_task_multi.py 924 66 &
-sleep 66
-nvidia-smi # let's see how the GPU is doing!
+# python vision/generateData/simple_task_multi.py 0 66 &
+# python vision/generateData/simple_task_multi.py 132 66 & 
+# python vision/generateData/simple_task_multi.py 198 66 &
+# python vision/generateData/simple_task_multi.py 264 66 &
+# python vision/generateData/simple_task_multi.py 330 66 &
+# python vision/generateData/simple_task_multi.py 396 66 &
+# python vision/generateData/simple_task_multi.py 462 66 &
+# python vision/generateData/simple_task_multi.py 528 66 &
+# python vision/generateData/simple_task_multi.py 660 66 &
+# python vision/generateData/simple_task_multi.py 726 66 &
+# python vision/generateData/simple_task_multi.py 792 66 &
+# python vision/generateData/simple_task_multi.py 858 66 &
+# python vision/generateData/simple_task_multi.py 924 66 &
+# sleep 66
+# nvidia-smi # let's see how the GPU is doing!
 # increase prob that this next 1 finishes last
-python vision/generateData/simple_task_multi.py 990 86
+# python vision/generateData/simple_task_multi.py 990 86
 
 # $? stores the exit code of the most recently finished process
-if [[ $? = 0 ]]; then
+# if [[ $? = 0 ]]; then
     # will check output for this to confirm success!
-    echo 69420
-fi
+    # echo 69420
+# fi
 
 # ensure everything is easily shared
-chmod -R 775 /nfs/hpc/share/$USER/mcs_opics
+# chmod -R 775 /nfs/hpc/share/$USER/mcs_opics
+
+# Training
+###########################################################################################3
+pip install cython &> /dev/null
+pip install opencv-python pillow pycocotools matplotlib &> /dev/null 
+pip install scikit-image &> /dev/null
+pip install scipy==1.2.0 &> /dev/null
+pip install tensorboardX &> /dev/null
+
+# From main mcs_opics
+cd visionmodule
+mkdir -p cd data/dataset/mcsvideo/interaction_scenes/
+
+cp -r /nfs/hpc/share/$USER/diego_bkup_2 data/dataset/mcsvideo/interaction_scenes/
+cp /nfs/hpc/share/$USER/diego_bkup_2/train.txt data/dataset/mcsvideo/interaction_scenes/
+cp /nfs/hpc/share/$USER/diego_bkup_2/eval.txt data/dataset/mcsvideo/interaction_scenes/
+
+python train.py --scripts=mcsvideo3_inter_unary_pw.json
+########################################################################################
 
 # kill the process keeping this slurm job open
 pgrep sleep | xargs ps | grep $MAX_TIME | xargs echo | cut -d ' ' -f 1 | xargs kill
