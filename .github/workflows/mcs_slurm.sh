fbde93ef2237571135a62b367f3304f083d8b4f9
Robert Bain <31903812+bainro@users.noreply.github.com>
2021-05-07 05:27:58 -0700

    Update mcs_slurm.sh

--- .github/workflows/mcs_slurm.sh
+++ .github/workflows/mcs_slurm.sh
@@ -9,12 +9,15 @@
 
 export MAX_TIME=1e4 # ~ 3hrs
 srun -N1 -n1 sleep $MAX_TIME &
+echo "test"
+
+module load anaconda
 module load gcc/6.5
 module load cuda
 nvidia-smi
+
 cd /scratch/MCS
 
-module load anaconda
 conda activate mcs_opics
 if [ $? -eq 0 ]; then
   :
