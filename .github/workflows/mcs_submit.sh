6097d9a38cc160df9833f9bda35a8e583deda4a6
Robert Bain <31903812+bainro@users.noreply.github.com>
2021-05-07 07:39:01 -0700

    Update mcs_submit.sh

--- .github/workflows/mcs_submit.sh
+++ .github/workflows/mcs_submit.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-source /etc/profile
-module load slurm
-
-chmod +x /tmp/mcs_slurm.sh
 SJOBID=$(sbatch --parsable /tmp/mcs_slurm.sh)
-
-echo $SJOBID
 
 # wait for gpu job to finish
 until [ `sacct -j $SJOBID | grep COMPLETED | wc -l` -gt 0 -o `sacct -j $SJOBID | grep CANCELLED | wc -l` -gt 0 ]
@@ -19,7 +13,7 @@ echo '==== STDOUT ===='
 cat mcs.out
 echo '==== STDERR ===='
 cat mcs.err
- 
+
 # check if the build failed/passed
 if ! grep 69420 mcs.out; 
   then exit 404; 
