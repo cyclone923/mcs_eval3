abb4ae8124e560ee294130a10013f73f404bb6f2
Robert Bain <31903812+bainro@users.noreply.github.com>
2021-05-10 03:55:32 -0700

    Update mcs_slurm.sh

--- .github/workflows/mcs_slurm.sh
+++ .github/workflows/mcs_slurm.sh
@@ -56,9 +56,9 @@ fi
 
 pip install -r requirements.txt &> /dev/null
 pip show machine_common_sense
-bash setup_torch.sh &> /dev/null
+bash setup_torch.sh
 bash setup_unity.sh &> /dev/null
-bash setup_vision.sh &> /dev/null
+bash setup_vision.sh
 export PYTHONPATH=$PWD
 
 python get_gravity_scenes.py &> /dev/null
