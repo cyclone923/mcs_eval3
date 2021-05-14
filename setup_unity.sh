9f7be084efa9f5e31b278a571493dc1546d17d0b
Robert Bain <31903812+bainro@users.noreply.github.com>
2021-05-14 05:43:14 -0700

    Update setup_unity.sh

--- setup_unity.sh
+++ setup_unity.sh
@@ -3,27 +3,32 @@
 mkdir -p unity_app
 cd unity_app
 
-export UNITY_VERSION=0.4.1
+export UNITY_VERSION=0.4.2
 # only download if not already installed
 if ! ls | grep $UNITY_VERSION; then
     if [ "$(uname)" == "Darwin" ]; then
         # Do something under Mac OS X platform
-        wget https://github.com/NextCenturyCorporation/MCS/releases/download/0.4.1/MCS-AI2-THOR-Unity-App-v0.4.1-mac.zip
-        wget https://github.com/NextCenturyCorporation/MCS/releases/download/0.4.1-1/UnityPlayer.so
-        unzip MCS-AI2-THOR-Unity-App-v0.4.1-mac.zip
-        rm MCS-AI2-THOR-Unity-App-v0.4.1-mac.zip
-        echo unity_path: \'unity_app/MCSai2thor.app/Contents/MacOS/MCSai2thor\' > ../unity_path.yaml
+        wget https://github.com/NextCenturyCorporation/MCS/releases/download/0.4.2/MCS-AI2-THOR-Unity-App-v0.4.2-mac.zip
+        wget https://github.com/NextCenturyCorporation/MCS/releases/download/0.4.2/UnityPlayer.so
+        unzip MCS-AI2-THOR-Unity-App-v0.4.2-mac.zip
+        rm MCS-AI2-THOR-Unity-App-v0.4.2-mac.zip
     elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
         # Do something under GNU/Linux platform
-        wget https://github.com/NextCenturyCorporation/MCS/releases/download/0.4.1/MCS-AI2-THOR-Unity-App-v0.4.1.x86_64
-        wget https://github.com/NextCenturyCorporation/MCS/releases/download/0.4.1-1/UnityPlayer.so
-        wget https://github.com/NextCenturyCorporation/MCS/releases/download/0.4.1/MCS-AI2-THOR-Unity-App-v0.4.1_Data.tar.gz
-        tar -xzvf MCS-AI2-THOR-Unity-App-v0.4.1_Data.tar.gz
-        rm MCS-AI2-THOR-Unity-App-v0.4.1_Data.tar.gz
-        chmod a+x MCS-AI2-THOR-Unity-App-v0.4.1.x86_64
-        echo unity_path: \'unity_app/MCS-AI2-THOR-Unity-App-v0.4.1.x86_64\' > ../unity_path.yaml
+        wget https://github.com/NextCenturyCorporation/MCS/releases/download/0.4.2/MCS-AI2-THOR-Unity-App-v0.4.2.x86_64
+        wget -O UnityPlayer.so https://github.com/NextCenturyCorporation/MCS/releases/download/0.4.2/UnityPlayer.so
+        wget https://github.com/NextCenturyCorporation/MCS/releases/download/0.4.2/MCS-AI2-THOR-Unity-App-v0.4.2_Data.tar.gz
+        tar -xzvf MCS-AI2-THOR-Unity-App-v0.4.2_Data.tar.gz
+        rm MCS-AI2-THOR-Unity-App-v0.4.2_Data.tar.gz
+        chmod a+x MCS-AI2-THOR-Unity-App-v0.4.2.x86_64
     fi
 fi
+
+if [ "$(uname)" == "Darwin" ]; then
+    echo unity_path: \'unity_app/MCSai2thor.app/Contents/MacOS/MCSai2thor\' > ../unity_path.yaml
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo unity_path: \'unity_app/MCS-AI2-THOR-Unity-App-v0.4.2.x86_64\' > ../unity_path.yaml
+fi
+
 
 echo [MCS] > ../mcs_config.ini
 echo debug = false >> ../mcs_config.ini
